56c3302a84edfedde0581f67e2b5cf0a05c06261
UnnayanBharadwaj <68640814+UnnayanBharadwaj@users.noreply.github.com>
2020-10-01 13:50:33 +0530

    comment Changes

--- ageGenderDetector.py
+++ ageGenderDetector.py
@@ -47,11 +47,14 @@ def startDetection():
 
             face = frame[y1:y2, x1:x2]
             blob=cv2.dnn.blobFromImage(face, 1.0, (227,227), MODEL_MEAN_VALUES, swapRB=False)
-
+            
+            # For Gender Predection
             genderNet.setInput(blob)
             genderPreds=genderNet.forward()
             gender=genderList[genderPreds[0].argmax()]
     
+            # For age Predection 
+ 
             #ageNet.setInput(blob)
             #agePreds=ageNet.forward()
             #age = getAge(ageList[agePreds[0].argmax()])
